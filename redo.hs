d69385a768504bccc0eaae0693ca09065dea2a79
dinkel <kevin.dinkel@jpl.nasa.gov>
2015-09-23 15:38:39 -0700

    started cleaning up

--- redo.hs
+++ redo.hs
@@ -15,17 +15,13 @@ main = mapM_ redo =<< getArgs
 
 redo :: String -> IO ()
 redo target = do 
-  let tmp3 = target ++ ".redo-tmp" -- this temp file gets passed as $3 and is written to by programs that do not print to stdout
-  let tmpStdout = target ++ ".redo-tmp2" -- this temp file captures what gets written to stdout
-  path <- redoPath target
-  case path of
-    Nothing -> error $ "No .do file found for target '" ++ target ++ "'"
-    Just path -> do
-      -- Pass redo script 3 arguments:
-      -- $1 - the target name
-      -- $2 - the target basename
-      -- $3 - the temporary target name
-      let command = "sh " ++ path ++ " " ++ target ++ " " ++ takeBaseName target ++ " " ++ tmp3 ++ " > " ++ tmpStdout
+  maybe printMissingPath redo' =<< redoPath target
+  where
+    printMissingPath = error $ "No .do file found for target '" ++ target ++ "'"
+    tmp3 = target ++ ".redo-tmp" -- this temp file gets passed as $3 and is written to by programs that do not print to stdout
+    tmpStdout = target ++ ".redo-tmp2" -- this temp file captures what gets written to stdout
+    redo' :: FilePath -> IO ()
+    redo' path = do
       (_, _, _, processHandle) <- createProcess $ shell $ command
       exit <- waitForProcess processHandle
       case exit of  
@@ -40,13 +36,16 @@ redo target = do
         ExitFailure code -> do hPutStrLn stderr $ "\n" ++ "Redo script exited with non-zero exit code: " ++ show code
       -- Remove the temporary files:
       tmp3Exists <- doesFileExist tmp3 
-      if tmp3Exists
-      then removeFile tmp3
-      else return ()
+      if tmp3Exists then removeFile tmp3 else return ()
       tmpStdoutExists <- doesFileExist tmpStdout 
-      if tmpStdoutExists 
-      then removeFile tmpStdout
-      else return ()
+      if tmpStdoutExists then removeFile tmpStdout else return ()
+      where 
+        -- Pass redo script 3 arguments:
+        -- $1 - the target name
+        -- $2 - the target basename
+        -- $3 - the temporary target name
+        command = "sh " ++ path ++ " " ++ target ++ " " ++ takeBaseName target ++ " " ++ tmp3 ++ " > " ++ tmpStdout
+       
 
 -- Take file path of target and return file path of redo script:
 redoPath :: FilePath -> IO (Maybe FilePath)
