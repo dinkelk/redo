aa8843a61de71c73cc6e7a94a8af517f9e10ebce
dinkel <kevin.dinkel@jpl.nasa.gov>
2015-09-23 14:47:09 -0700

    redo now uses two temporary files for correct implementation

--- redo.hs
+++ redo.hs
@@ -1,4 +1,5 @@
 import Control.Monad (filterM)
+import Control.Exception (catch, SomeException)
 import System.Directory (renameFile, removeFile, doesFileExist)
 import System.Environment (getArgs)
 import System.Exit (ExitCode(..))
@@ -13,18 +14,34 @@ main = do
 
 redo :: String -> IO ()
 redo target = do 
-  let tmp = target ++ ".redo-tmp"
+  let tmp3 = target ++ ".redo-tmp" -- this temp file gets passed as $3 and is written to by programs that do not print to stdout
+  let tmpStdout = target ++ ".redo-tmp2" -- this temp file captures what gets written to stdout
   path <- redoPath target
   case path of
     Nothing -> error $ "No .do file found for target '" ++ target ++ "'"
     Just path -> do
-      let command = "sh " ++ path ++ " - - " ++ tmp
+      let command = "sh " ++ path ++ " - - " ++ tmp3 ++ " > " ++ tmpStdout
       (_, _, _, processHandle) <- createProcess $ shell $ command
       exit <- waitForProcess processHandle
-      case exit of 
-        ExitSuccess -> renameFile tmp target 
+      case exit of  
+        -- try to copy temp3 file
+        -- if that fails, then copy the tempStdout file
+        ExitSuccess -> do catch (renameFile tmp3 target) handler1
+                          where
+                            handler1 :: SomeException -> IO ()
+                            handler1 ex = do catch (renameFile tmpStdout target) handler2
+                            handler2 :: SomeException -> IO ()
+                            handler2 ex = do hPutStrLn stderr $ "Redo could not copy results from temporary file"
         ExitFailure code -> do hPutStrLn stderr $ "\n" ++ "Redo script exited with non-zero exit code: " ++ show code
-                               removeFile tmp
+      -- Remove the temporary files:
+      tmp3Exists <- doesFileExist tmp3 
+      if tmp3Exists
+      then removeFile tmp3
+      else return ()
+      tmpStdoutExists <- doesFileExist tmpStdout 
+      if tmpStdoutExists 
+      then removeFile tmpStdout
+      else return ()
 
 -- Take file path of target and return file path of redo script:
 redoPath :: FilePath -> IO (Maybe FilePath)
