64f535d469adb77fa711b47d07f4eceb8b942888
dinkel <kevin.dinkel@jpl.nasa.gov>
2015-09-23 00:38:48 -0700

    redo now makes file into temporary file and then atomically copied it to target

--- redo.do
+++ redo.do
@@ -1,1 +1,5 @@
-ghc redo.hs
+#!/bin/sh
+
+# Compile redo.hs to filename $3 with verbosity set to low 
+# so there is nothing printed to standard out
+ghc -v0 -o $3 redo.hs
--- redo.hs
+++ redo.hs
@@ -1,4 +1,9 @@
+import Control.Monad (filterM)
+import System.Directory (renameFile, removeFile, doesFileExist)
 import System.Environment (getArgs)
+import System.Exit (ExitCode(..))
+import System.FilePath (replaceBaseName, hasExtension)
+import System.IO (hPutStrLn, stderr)
 import System.Process (createProcess, waitForProcess, shell)
 
 main :: IO ()
@@ -8,6 +13,24 @@ main = do
 
 redo :: String -> IO ()
 redo target = do 
-  (_, _, _, processHandle) <- createProcess $ shell $ "sh " ++ target ++ ".do"
-  _ <- waitForProcess processHandle
-  return ()
+  let tmp = target ++ ".redo-tmp"
+  path <- redoPath target
+  case path of
+    Nothing -> error $ "No .do file found for target '" ++ target ++ "'"
+    Just path -> do
+      let command = "sh " ++ path ++ " - - " ++ tmp
+      (_, _, _, processHandle) <- createProcess $ shell $ command
+      exit <- waitForProcess processHandle
+      case exit of 
+        ExitSuccess -> renameFile tmp target 
+        ExitFailure code -> do hPutStrLn stderr $ "\n" ++ "Redo script exited with non-zero exit code: " ++ show code
+                               removeFile tmp
+
+-- Take file path of target and return file path of redo script:
+redoPath :: FilePath -> IO (Maybe FilePath)
+redoPath target = do 
+  existingCandidates <- filterM doesFileExist candidates
+  return $ safeHead existingCandidates
+  where candidates =  [target ++ ".do"] ++ if hasExtension target then [replaceBaseName target "default.do"] else []
+        safeHead [] = Nothing
+        safeHead (x:_) = Just x
