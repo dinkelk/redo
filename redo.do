89a25beee99be19336dc61a01052ad33f9101a6e
dinkel <kevin.dinkel@jpl.nasa.gov>
2015-09-27 08:31:01 -0700

    adding REDO_TARGET var

--- redo.do
+++ redo.do
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Dependencies:
+redo-ifchange redo.hs
+
 # Compile redo.hs to filename $3 with verbosity set to low 
 # so there is nothing printed to standard out
 ghc -v0 -o $3 redo.hs
--- redo.hs
+++ redo.hs
@@ -1,13 +1,26 @@
+-- adding StandAloneDeriving extension:
+{-# LANGUAGE StandaloneDeriving #-}
+
 import Control.Monad (filterM, liftM)
 import Control.Exception (catch, SomeException)
+import Data.Map.Lazy (adjust, insert, fromList, toList)
 import Data.Maybe (listToMaybe)
+import Debug.Trace (traceShow)
 import System.Directory (renameFile, removeFile, doesFileExist)
-import System.Environment (getArgs)
+import System.Environment (getArgs, getEnvironment)
 import System.Exit (ExitCode(..))
 import System.FilePath (replaceBaseName, hasExtension, takeBaseName)
 import System.IO (hPutStrLn, stderr)
-import System.Process (createProcess, waitForProcess, shell)
+import System.Process (createProcess, waitForProcess, shell, CreateProcess(..), StdStream(..), CmdSpec(..))
 import Data.Bool (bool)
+
+-- Make using trace easier, for debugging only:
+traceShow' arg = traceShow arg arg
+
+-- We want to be able to show create process:
+deriving instance Show CreateProcess
+deriving instance Show StdStream
+deriving instance Show CmdSpec
 
 main :: IO ()
 --main = do
@@ -22,7 +35,10 @@ redo target = do
     printMissingPath = error $ "No .do file found for target '" ++ target ++ "'"
     redo' :: FilePath -> IO ()
     redo' path = do
-      (_, _, _, processHandle) <- createProcess $ shell $ command
+      -- Add REDO_TARGET to environment, and make sure there is only one REDO_TARGET in the environment
+      oldEnv <- getEnvironment
+      let newEnv = toList $ adjust (++ ":.") "PATH" $ insert "REDO_TARGET" target $ fromList oldEnv
+      (_, _, _, processHandle) <- createProcess $ traceShow' $ (shell $ command) {env = Just newEnv}
       exit <- waitForProcess processHandle
       case exit of  
         ExitSuccess -> do catch (renameFile tmp3 target) handler1
@@ -38,7 +54,7 @@ redo target = do
         -- $1 - the target name
         -- $2 - the target basename
         -- $3 - the temporary target name
-        command = unwords ["sh ", path, target, takeBaseName target, tmp3, ">", tmpStdout]
+        command = traceShow' $ unwords ["sh", path, target, takeBaseName target, tmp3, ">", tmpStdout]
         -- If renaming the tmp3 fails, let's try renaming tmpStdout:
         handler1 :: SomeException -> IO ()
         handler1 ex = do catch (renameFile tmpStdout target) handler2
