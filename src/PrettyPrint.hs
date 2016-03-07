9e6d110a50b0acd583b92dad2bc1c5694679fb2f
dinkel <kevin.dinkel@jpl.nasa.gov>
2016-03-06 17:31:05 -0800

    simplifying pretty print, it performs much better in a multi-threaded environment now

--- src/PrettyPrint.hs
+++ src/PrettyPrint.hs
@@ -1,46 +1,42 @@
 
 module PrettyPrint(putInfoStrLn, putWarningStrLn, putErrorStrLn, putStatusStrLn, putRedoStatus, putUnformattedStrLn) where
 
-import System.Console.ANSI (hSetSGR, SGR(..), ConsoleLayer(..), Color(..), ColorIntensity(..), ConsoleIntensity(..))
-import System.IO (hPutStrLn, hPutStr, stderr)
+import System.IO (hPutStrLn, stderr, hFlush, hSetBuffering, BufferMode(..), stdout)
 
--- Set colors and write some text in those colors.
-setConsoleDefault :: IO ()
-setConsoleDefault = hSetSGR stderr [Reset]
-setConsoleColor :: Color -> IO ()
-setConsoleColor color = hSetSGR stderr [SetColor Foreground Vivid color] 
-setConsoleBold :: IO ()
-setConsoleBold = hSetSGR stderr [SetConsoleIntensity BoldIntensity]
-setConsoleFaint :: IO ()
-setConsoleFaint = hSetSGR stderr [SetConsoleIntensity FaintIntensity]
-setConsoleColorDull :: Color -> IO ()
-setConsoleColorDull color = hSetSGR stderr [SetColor Foreground Dull color] 
+-- ANSI color definitions:
+-- TODO make these "" if terminal is not smart
+red :: String
+red = "\x1b[31m"
+green :: String
+green = "\x1b[32m"
+yellow :: String
+yellow = "\x1b[33m"
+cyan :: String
+cyan = "\x1b[36m"
+bold :: String
+bold = "\x1b[1m"
+plain :: String
+plain = "\x1b[m"
 
 -- Put string to console in color:
-putColorStrLn :: Color -> String -> IO ()
-putColorStrLn color string = do setConsoleColor color
-                                setConsoleBold 
-                                hPutStrLn stderr string
-                                setConsoleDefault 
+putColorStrLn :: String -> String -> IO ()
+putColorStrLn color string = hPutStrLn stderr $ color ++ bold ++ string ++ plain
 
 -- Put info, warning, error strings to console:
 putUnformattedStrLn :: String -> IO ()
 putUnformattedStrLn = hPutStrLn stderr
 putInfoStrLn :: String -> IO ()
-putInfoStrLn = putColorStrLn Green 
+putInfoStrLn = putColorStrLn green 
 putWarningStrLn :: String -> IO ()
-putWarningStrLn = putColorStrLn Yellow 
+putWarningStrLn = putColorStrLn yellow 
 putErrorStrLn :: String -> IO ()
-putErrorStrLn = putColorStrLn Red 
+putErrorStrLn = putColorStrLn red 
 putStatusStrLn :: String -> IO ()
-putStatusStrLn = putColorStrLn Cyan
+putStatusStrLn = putColorStrLn cyan
 
 -- Special function to format and print the redo status message of what is being built:
 putRedoStatus :: Int -> FilePath -> IO ()
-putRedoStatus depth file = do setConsoleColorDull Green 
-                              setConsoleFaint
-                              hPutStr stderr $ "redo  " ++ concat (replicate depth "  " )
-                              setConsoleColor Green
-                              setConsoleBold
-                              hPutStrLn stderr file
-                              setConsoleDefault
+putRedoStatus depth file = do hSetBuffering stderr LineBuffering
+                              hFlush stdout
+                              hPutStrLn stderr $ green ++ "redo  " ++ concat (replicate depth "  " ) ++ bold ++ file ++ plain
+                              hFlush stderr
