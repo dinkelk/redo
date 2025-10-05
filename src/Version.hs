76f28f74752b57608dd277670fa356a980382acf
Kevin Dinkel <1225857+dinkelk@users.noreply.github.com>
2025-10-05 07:50:39 -0600

    Update version output format

---
+++ src/Version.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Version (versionString) where
+
+import qualified System.Info
+import BuildInfo (gitCommitHash, buildTimestamp)
+
+-- Version string with platform, commit, and build time
+versionString :: String
+versionString = "redo 0.1 (" ++ System.Info.os ++ "-" ++ System.Info.arch ++ ")\n" ++
+                "https://github.com/dinkelk/redo\n" ++
+                "commit: " ++ $(gitCommitHash) ++ "\n" ++
+                "built: " ++ $(buildTimestamp)
